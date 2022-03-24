be19ee0288f389c37e3e7a492d62c6a4bc8fc01e
Jorge <jorgeMGdev@gmail.com>
2022-03-24 12:33:28 -0700

    icon navbar added

--- pages/welcome.js
+++ pages/welcome.js
@@ -1,27 +1,41 @@
+import Image from "next/image";
+import { FaGithub, FaGitlab, FaLinkedin } from 'react-icons/fa'
+
 export default function Welcome() {
   return (
-    <div className="font-main justify-center max-w-3xl mx-auto">
-      <div className="w-full p-2 border border-white mt-5">
-        <ul className="flex text-white justify-between px-5 items-center">
+    <div>
+      <div className="font-main justify-center max-w-3xl mx-auto">
+        <div className="w-full p-2 border border-white mt-5">
+          <ul className="flex text-white justify-between px-5 items-center">
+            <a href="/welcome">
+              <li className="text-2xl">Welcome</li>
+            </a>
+            <a href="/welcome">
+              <li>Projects</li>
+            </a>
+            <a href="/welcome">
+              <li>Projects</li>
+            </a>
+            <a href="/welcome">
+              <li>Projects</li>
+            </a>
+            <a href="/welcome">
+              <li>Projects</li>
+            </a>
+          </ul>
+        </div>
+        <ul className="flex flex-col h-1/4 fixed right-0 text-white justify-between px-5 top-1/3 items-center pl-2">
           <a href="/welcome">
-            <li className="text-2xl">Welcome</li>
+						<FaGithub size='2rem'/>
           </a>
           <a href="/welcome">
-            <li>Projects</li>
+						<FaGitlab size='2rem'/>
           </a>
           <a href="/welcome">
-            <li>Projects</li>
-          </a>
-          <a href="/welcome">
-            <li>Projects</li>
-          </a>
-          <a href="/welcome">
-            <li>Projects</li>
+						<FaLinkedin size='2rem'/>
           </a>
         </ul>
       </div>
     </div>
   );
 }
-
-
