83723bc08542905b3f76fb86146d7468cb1cb2b5
Jorge <jorgeMGdev@gmail.com>
2022-03-28 23:06:17 -0700

    empty tag on contactMeButton

--- components/contactMeButton/index.js
+++ components/contactMeButton/index.js
@@ -5,9 +5,7 @@ export const ContactMeButtonSection = () => {
     <div className="w-full justify-center text-center text-harder-text-color">
       <h5 className="text-xl pb-4">Interested?</h5>
       <Link href="/contact-me">
-        <a
-          href="/contact-me"
-          className="border border-harder-text-color p-2 px-4 font-semibold">
+        <a className="border border-harder-text-color p-2 px-4 font-semibold">
           Contact me
         </a>
       </Link>
