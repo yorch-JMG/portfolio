f628d8e4e0bb8e42794995b3f640bb72a9354f15
Jorge <jorgeMGdev@gmail.com>
2022-03-29 09:50:44 -0700

    color fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,9 @@ module.exports = {
   theme: {
 		colors: {
 			'background' : '#000000',
-			'brown' : '#f5cf51',
+			'brown' : '#ffae00',
 			'dark-navy-blue' : '#22333b',
-			'harder-text-color' : '#e3ae1e',
+			'harder-text-color' : '#ffee00',
 			'base-text-color' : 'white'
 		},
 		extend: {
