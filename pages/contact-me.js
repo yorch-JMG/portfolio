3fde3495cdcb43d752ab9838e5e71c6ae5df98ed
Jorge <jorgeMGdev@gmail.com>
2022-03-28 19:39:26 -0700

    contact-me page

--- pages/contact-me.js
+++ pages/contact-me.js
@@ -1,9 +1,15 @@
-import {Navbars} from '../components/navbars'
+import {Navbars} from '../components/navbars';
 export default function ContactMe() {
   return (
     <div className="max-w-3xl mx-auto z-20">
-			<Navbars />
-			ContactMe
+      <Navbars />
+      <div className="pt-5 flex h-fit">
+        <div className="text-base-text-color">
+          <h2 className="text-3xl">Contact me</h2>
+          <input type="text" placeholder="Enter your name"></input>
+        </div>
+        <div className="bg-dark-navy-blue h-20 w-7"></div>
+      </div>
     </div>
   );
 }
