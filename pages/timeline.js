3a537ca6b91f10f7a4e02b9545b6b29dd8a47932
Jorge <jorgeMGdev@gmail.com>
2022-04-04 16:40:00 -0700

    timeline page responsive

--- pages/timeline.js
+++ pages/timeline.js
@@ -1,5 +1,5 @@
 import {Navbars} from '../components/navbars';
-import {timelineObjects} from '../components/timelineObj'
+import {timelineObjects} from '../components/timelineObj';
 
 export default function Timeline() {
   return (
@@ -8,33 +8,33 @@ export default function Timeline() {
       <h1 className="text-base-text-color text-5xl font-bold px-5 pt-6">
         Timeline
       </h1>
-      <div className="w-full px-5">
+      <div className="w-full px-3 md:px-5">
         {timelineObjects.map(timelineObject => {
           return (
             <>
               <div>
-                <div className="flex justify-between pt-10 align-middle items-center">
-                  <div className="flex space-x-3 items-center">
-                    <h1 className="text-base-text-color font-bold text-3xl">
+						<div className="flex flex-col md:flex-row pt-10 md:justify-between md:align-middle md:items-center">
+                  <div className="flex flex-col md:justify-between">
+                    <h1 className="text-base-text-color font-bold flex-wrap text-2xl md:text-3xl pb-1 md:pb-0">
                       {timelineObject.title}
                     </h1>
-                    <h2 className="text-background bg-harder-text-color font-bold p-1 px-2 rounded-md">
+                    <h2 className="text-background bg-harder-text-color font-bold p-1 px-2 rounded-md w-fit">
                       {timelineObject.location}
                     </h2>
                   </div>
-                  <h2 className="text-base-text-color font-bold text-xl">
+                  <h2 className="text-base-text-color font-bold text-sm py-1 md:pt-0 md:text-xl pt-2">
                     {timelineObject.date}
                   </h2>
                 </div>
                 <div>
                   {timelineObject.projects.map(project => {
                     return (
-                      <div className="pl-5 pt-3">
-                        <div className="flex justify-between">
+                      <div className="pl-5 pt-2 md:pt-3">
+                        <div className="flex flex-col md:flex-row justify-between">
                           <h1 className="text-base-text-color font-bold text-2xl">
                             {project.name}
                           </h1>
-                          <h2 className="text-base-text-color font-semibold text-lg">
+                          <h2 className="text-base-text-color font-semibold md:text-lg">
                             {project.date}
                           </h2>
                         </div>
